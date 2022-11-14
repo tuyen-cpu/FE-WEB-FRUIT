5683bf6a372a245cb755237bb882b3c2d85b5443
tuyen-cpu <tranquangtuyen326326@gmail.com>
2022-11-14 22:32:32 +0700

    slide show home page, check validate checkout button

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,20 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {ButtonModule} from 'primeng/button';
+import { ButtonModule } from 'primeng/button';
 import { RouterModule } from '@angular/router';
+
+import { CarouselModule } from 'primeng/carousel';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,ButtonModule,RouterModule],
+  imports: [CommonModule, ButtonModule, RouterModule, CarouselModule],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent implements OnInit {
+  backgrounds: any = [
+    'slideshow_1.webp',
+    'slideshow_2.webp',
+    'slideshow_3.webp',
+  ];
+  categories: any = ['1', '2', '3', '4'];
+  responsiveOptions: any;
+  responsiveCateOptions: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.responsiveOptions = [
+      {
+        breakpoint: '1024px',
+        numVisible: 1,
+        numScroll: 3,
+      },
+      {
+        breakpoint: '768px',
+        numVisible: 1,
+        numScroll: 3,
+      },
+      {
+        breakpoint: '560px',
+        numVisible: 1,
+        numScroll: 3,
+      },
+    ];
+    this.responsiveCateOptions = [
+      {
+        breakpoint: '1024px',
+        numVisible: 2,
+        numScroll: 3,
+      },
+      {
+        breakpoint: '768px',
+        numVisible: 1,
+        numScroll: 3,
+      },
+      {
+        breakpoint: '560px',
+        numVisible: 1,
+        numScroll: 3,
+      },
+    ];
   }
-
 }
